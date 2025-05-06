bb35c868ce16c13bcaf47e5e3014acd36bc5a405
Leonardo Zancanaro <leonardobzancanaro@gmail.com>
2025-05-06 19:37:35 -0300

    vers 3 testes debug

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-print('teste')
+
+#Código utilizado na aula de debug:
+#1: marcar linha break point
+#2: F5 -> run and debug
+#3: Avançar linha a linha
+
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+
+    total = pizza + refrigerante
+    #total = str(pizza) + refrigerante
+
+    print(total)
+
+#calcular_preco_combo(30, 'vinte reais')
+calcular_preco_combo(30, 20)
+
+print('Programa finalizado')
